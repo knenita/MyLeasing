14dbc4b14166ba1f7fd16f2e460924263c2683b1
Diana Russi <diana.russiposada@hotmail.com>
2020-03-04 13:23:28 -0500

    Add image model

--- MyLeasing.Web/Startup.cs
+++ MyLeasing.Web/Startup.cs
@@ -50,6 +50,7 @@ namespace MyLeasing.Web
             services.AddScoped<IUserHelper, UserHelper>();
             services.AddScoped<ICombosHelper, CombosHelper>();
             services.AddScoped<IConverterHelper, ConverterHelper>();
+            services.AddScoped<IImageHelper, ImageHelper>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
